c1f2807f60b014ebd3a1974238f605f66a6ed810
Kevin Liao <liaok0082@gmail.com>
2021-05-18 16:56:10 +0800

    Postgres deleted

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,41 +1,20 @@
 import { Module, Global } from '@nestjs/common';
-import { Client } from 'pg';
 import { MongoClient } from 'mongodb';
-
-const client = new Client({
-    user: process.env.DATABASE_USER, 
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME, 
-    password: process.env.DATABASE_PSW,
-    port: parseInt(process.env.DATABASE_PORT),
-  })
-  
-  client.connect(); 
 
 @Global() 
 @Module({
     providers: [
         {
-            provide: 'PG',
-            useValue: client
-        },
-        {
             provide: 'MONGO',
             useFactory: async () => {
-                console.log('Entrando a mongo')
                 const url = process.env.MONGO_URL; 
-                console.log('Crendo el cleinte')
                 const client = new MongoClient(url, {useUnifiedTopology: true});
-                console.log('Cree el cliente')
                 await client.connect();
-                console.log('Conecte')
                 const database = client.db('parkings');
-                console.log('La base de datos')
-                console.log(database);
                 return database; 
             }
         }
     ], 
-    exports: ['MONGO', 'PG']
+    exports: ['MONGO']
 })
 export class DatabaseModule {}
--- src/parkings/parking.module.ts
+++ src/parkings/parking.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { ParkingsController } from './controllers/parkings.controller';
-import { AddressesController } from './controllers/addresses.controller';
-import { LocationsController } from './controllers/locations.controller';
 
 import { ParkingsService } from './services/parkings.service';
-import { AddressService } from './services/addresses.service';
-import { LocationService } from './services/locations.service';
 
 @Module({
-    controllers: [ParkingsController, AddressesController, LocationsController], 
-    providers: [ParkingsService, AddressService, LocationService],
+    controllers: [ParkingsController], 
+    providers: [ParkingsService],
 })
 export class ParkingModule {}
