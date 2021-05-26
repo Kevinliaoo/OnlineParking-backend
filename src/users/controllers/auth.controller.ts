6c424c3b755c613963cc78e0b3462d022da3b5f4
Kevin Liao <liaok0082@gmail.com>
2021-05-26 14:43:29 +0800

    Pipes fixed

--- src/users/controllers/auth.controller.ts
+++ src/users/controllers/auth.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Post, UseGuards, Body, Req } from '@nestjs/common'; 
+import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common'; 
 import { AuthGuard } from '@nestjs/passport';
-import Request from 'express';
+import { Request } from 'express';
 
 import { AuthService } from '../services/auth.service';
 import { User } from '../entities/user.entity';
-import { LoginDto } from '../dtos/auth.dto';
-
-import { ParseAuthPipe } from '../../common/parse-user.pipe';
 
 @Controller('auth')
 export class AuthController {
@@ -15,14 +12,8 @@ export class AuthController {
 
     // Login middleware
     @UseGuards(AuthGuard('local'))
-    @Post('/loginn')
-    llogin(@Req() req: Request) {
-        return ("vdfjfmsdklo")
-        // return this.authService.generateJwt(req.user as User);
-    }
-
     @Post('/login')
-    login(@Body(ParseAuthPipe) user: LoginDto) {
-        return this.authService.generateJwt(user as User);
+    login(@Req() req: Request) {
+        return this.authService.generateJwt(req.user as User);
     }
 }
